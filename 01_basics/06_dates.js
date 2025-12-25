513a38dd78209f0e8e5c928a3fa65fd81cd30516
Anurag Dubey <mr.anurag9628@gmail.com>
2025-12-25 22:45:34 +0530

    Dates in js

---
+++ 01_basics/06_dates.js
@@ -0,0 +1,11 @@
+// Date and Time Manipulation in JavaScript
+
+let myDate = new Date();
+console.log(myDate); // Current date and time
+console.log(typeof myDate); // object 
+
+
+console.log(myDate.toString()); // Full date string
+console.log(myDate.toDateString()); // Date portion only
+console.log(myDate.toTimeString()); // Time portion only
+
